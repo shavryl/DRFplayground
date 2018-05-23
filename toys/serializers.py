9015c661b7303e499bde3668606ae1017c4c4c54
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2018-05-23 08:52:40 +0300

    serializers and views

---
+++ toys/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from toys.models import Toy
+
+
+class ToySerializer(serializers.Serializer):
+
+    pk = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(max_length=15)
+    description = serializers.CharField(max_length=250, blank=True, default='')
+    release_date = serializers.DateTimeField()
+    toy_category = serializers.CharField(max_length=200, blank=False, default='')
+    was_included_in_home = serializers.BooleanField(default=False)
+
+    def create(self, validated_data):
+
+        return Toy.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+
+        instance.name = validated_data.get('name', instance.name)
+        instance.description = validated_data.get('description', instance.description)
+        instance.release_date = validated_data.get('release_date', instance.release_date)
+        instance.toy_category = validated_data.get('toy_category', instance.toy_category)
+        instance.was_included_in_home = validated_data.get('was_included_in_home',
+                                                           instance.was_included_in_home)
+        instance.save()
+        return instance
