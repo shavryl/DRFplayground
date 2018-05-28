5afa6483af2faee89d9c01b3549a7d5ff0aef48f
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2018-05-28 15:02:09 +0300

    model serializer

--- toys/serializers.py
+++ toys/serializers.py
@@ -2,26 +2,9 @@ from rest_framework import serializers
 from toys.models import Toy
 
 
-class ToySerializer(serializers.Serializer):
+class ToySerializer(serializers.ModelSerializer):
 
-    pk = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(max_length=15)
-    description = serializers.CharField(max_length=250)
-    release_date = serializers.DateTimeField()
-    toy_category = serializers.CharField(max_length=200)
-    was_included_in_home = serializers.BooleanField(default=False)
-
-    def create(self, validated_data):
-
-        return Toy.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-
-        instance.name = validated_data.get('name', instance.name)
-        instance.description = validated_data.get('description', instance.description)
-        instance.release_date = validated_data.get('release_date', instance.release_date)
-        instance.toy_category = validated_data.get('toy_category', instance.toy_category)
-        instance.was_included_in_home = validated_data.get('was_included_in_home',
-                                                           instance.was_included_in_home)
-        instance.save()
-        return instance
+    class Meta:
+        model = Toy
+        fields = ('id', 'name', 'description', 'release_date',
+                  'toy_category', 'was_included_in_home')
