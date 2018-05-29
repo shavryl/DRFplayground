b9ac5321249f8668e3ce3c6f1779538ce519d372
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2018-05-29 08:29:47 +0300

    models Pilot, Competition

--- drones/models.py
+++ drones/models.py
@@ -31,3 +31,34 @@ class Drone(models.Model):
         return self.name
 
 
+class Pilot(models.Model):
+
+    MALE = 'M'
+    FEMALE = 'F'
+    GENDER_CHOICES = (
+        (MALE, 'Male'),
+        (FEMALE, 'Female'),)
+
+    name = models.CharField(max_length=150, blank=False, default='')
+    gender = models.CharField(
+        max_length=2, choices=GENDER_CHOICES, default=MALE,)
+    races_count = models.IntegerField()
+    inserted_timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = 'name'
+
+    def __str__(self):
+        return self.name
+
+
+class Competition(models.Model):
+
+    pilot = models.ForeignKey(
+        Pilot, related_name='competitions', on_delete=models.CASCADE)
+    drone = models.ForeignKey(Drone, on_delete=models.CASCADE)
+    distance_in_feet = models.IntegerField()
+    distance_achievement_date = models.DateTimeField()
+
+    class Meta:
+        ordering = '-distance_in_feet'
