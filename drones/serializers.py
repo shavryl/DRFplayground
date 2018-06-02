3f65f83bc003b0ae23791905ef1a4a34a50d54ba
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2018-06-02 10:04:28 +0300

    serializers all

--- drones/serializers.py
+++ drones/serializers.py
@@ -53,3 +53,15 @@ class PilotSerializer(serializers.HyperlinkedModelSerializer):
                   'races_count', 'inserted_timestamp', 'competitions')
 
 
+class PilotCompetitionSerializer(serializers.ModelSerializer):
+
+    pilot = serializers.SlugRelatedField(
+        queryset=Pilot.objects.all(), slug_field='name')
+    drone = serializers.SlugRelatedField(
+        queryset=Drone.objects.all(), slug_field='name')
+
+    class Meta:
+
+        model = Competition
+        fields = ('url', 'pk', 'distance_in_feet',
+                  'distance_achievement_date', 'pilot', 'drone')
