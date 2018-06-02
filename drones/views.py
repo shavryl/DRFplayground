026a39a91d65eead134fb633e11e3df8ddd7a361
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2018-06-02 10:16:08 +0300

    views for drones

--- drones/views.py
+++ drones/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from drones.models import DroneCategory
+from drones.models import Drone
+from drones.models import Pilot
+from drones.models import Competition
+from drones.serializers import DroneCategorySerializer
+from drones.serializers import DroneSerializer
+from drones.serializers import PilotSerializer
+from drones.serializers import PilotCompetitionSerializer
 
-# Create your views here.
+
+class DroneCategoryList(generics.ListCreateAPIView):
+
+    queryset = DroneCategory.objects.all()
+    serializer_class = DroneCategorySerializer
+    name = 'dronecategory-list'
+
+
+class DroneCategoryDetail(generics.RetrieveUpdateDestroyAPIView):
+
+    queryset = DroneCategory.objects.all()
+    serializer_class = DroneCategorySerializer
+    name = 'dronecategory-detail'
+
+
+class DroneList(generics.ListCreateAPIView):
+
+    queryset = Drone.objects.all()
+    serializer_class = DroneSerializer
+    name = 'drone-list'
+
+
