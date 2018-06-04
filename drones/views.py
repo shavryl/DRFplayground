4cdcdfe01020590b29f846a558a9d5f952758733
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2018-06-04 07:37:21 +0300

    generic views

--- drones/views.py
+++ drones/views.py
@@ -33,3 +33,36 @@ class DroneList(generics.ListCreateAPIView):
     name = 'drone-list'
 
 
+class DroneDetail(generics.RetrieveUpdateDestroyAPIView):
+
+    queryset = Drone.objects.all()
+    serializer_class = DroneSerializer
+    name = 'drone-detail'
+
+
+class PilotList(generics.ListCreateAPIView):
+
+    queryset = Pilot.objects.all()
+    serializer_class = PilotSerializer
+    name = 'pilot-list'
+
+
+class PilotDetail(generics.RetrieveUpdateDestroyAPIView):
+
+    queryset = Pilot.objects.all()
+    serializer_class = PilotSerializer
+    name = 'pilot-detail'
+
+
+class CompetitionList(generics.ListCreateAPIView):
+
+    queryset = Competition.objects.all()
+    serializer_class = PilotCompetitionSerializer
+    name = 'competition-list'
+
+
+class CompetitionDetail(generics.RetrieveUpdateDestroyAPIView):
+
+    queryset = Competition.objects.all()
+    serializer_class = PilotCompetitionSerializer
+    name = 'competition-detail'
