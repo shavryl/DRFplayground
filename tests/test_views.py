578e70d28793dcb9699c8e4cc10ec458aeeba81f
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2018-06-08 08:25:56 +0300

    test views

--- tests/test_views.py
+++ tests/test_views.py
@@ -19,7 +19,6 @@ class DroneCategoryTest(APITestCase):
 
         new_drone_category_name = 'Hexacopter'
         response = self.post_drone_category(new_drone_category_name)
-
         assert response.status_code == status.HTTP_201_CREATED
         assert DroneCategory.objects.count() == 1
         assert DroneCategory.objects.get().name == new_drone_category_name
@@ -50,3 +49,17 @@ class DroneCategoryTest(APITestCase):
         assert response.status_code == status.HTTP_200_OK
         assert response.data['count'] == 1
         assert response.data['results'][0]['name'] == drone_category_name1
+
+    def test_update_drone_category(self):
+
+        drone_category_name = 'Category Initial Name'
+        response = self.post_drone_category(drone_category_name)
+        url = reverse(views.DroneCategoryDetail.name,
+                      None, {response.data['pk']})
+        updated_drone_category_name = 'Updated Name'
+        data = {'name': updated_drone_category_name}
+        patch_response = self.client.patch(url, data, format='json')
+
+        assert patch_response.status_code == status.HTTP_200_OK
+        assert patch_response.data['name'] == updated_drone_category_name
+
