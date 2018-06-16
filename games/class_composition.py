e6cdd2be11393bf609f2203b308f206a6adc5551
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2018-06-16 20:15:03 +0300

    composition department

--- games/class_composition.py
+++ games/class_composition.py
@@ -17,12 +17,12 @@ class Person:
         return '[Person: %s, %s]' % (self.name, self.pay)
 
 
-class Manager:
+class Manager(Person):
 
     def __init__(self, name, pay):
         self.person = Person(name, 'mgr', pay)
 
-    def give_raise(self, percent, bonus):
+    def give_raise(self, percent, bonus=.10):
         self.person.give_raise(percent + bonus)
 
     def __getattr__(self, attr):
@@ -32,6 +32,32 @@ class Manager:
         return str(self.person)
 
 
+class Department:
+
+    def __init__(self, *args):
+        self.members = list(args)
+
+    def add_member(self, person):
+        self.members.append(person)
+
+    def give_raise(self, percent):
+        for person in self.members:
+            person.give_raise(percent)
+
+    def show_all(self):
+        for person in self.members:
+            print(person)
+
+
+
+
+
 if __name__ == '__main__':
-    tom = Manager('Tom HArdy', 177)
-    tom.give_raise(10, 10)
+    bob = Person('Bob Smith')
+    sue = Person('Sue Jones', job='dev', pay=100000)
+    tom = Manager('Tom Jones', 50000)
+
+    development = Department(bob, sue)  # Embed objects in a composite
+    development.add_member(tom)
+    development.give_raise(.10)  # Runs embedded objects' giveRaise
+    development.show_all()  # Runs embedded objects' __repr__
