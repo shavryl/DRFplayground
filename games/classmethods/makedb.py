0fccf319d9941d4e93c91d6111368a3ee1041a84
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2018-06-17 20:30:48 +0300

    shelves and pickles

---
+++ games/classmethods/makedb.py
@@ -0,0 +1,14 @@
+from games.classmethods.class_reference import Person, Manager
+
+
+bob = Person('Bob Smith')
+sue = Person('Sue Jones', job='dev', pay=100000)
+tom = Manager('Tom Jones', 50000)
+
+
+import shelve
+
+db = shelve.open('pesondb')
+for obj in (bob, sue, tom):
+    db[obj.name] = obj
+db.close()
