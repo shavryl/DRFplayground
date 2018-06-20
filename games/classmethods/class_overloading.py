d17e79ad8f89dbee5624e47bfc3c8165cffded8c
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2018-06-20 09:27:05 +0300

    Iters and contains

--- games/classmethods/class_overloading.py
+++ games/classmethods/class_overloading.py
@@ -6,22 +6,11 @@ class SkipObject:
         self.wrapped = wrapped
 
     def __iter__(self):
-        return SkipIterator(self.wrapped)
-
-
-class SkipIterator:
-
-    def __init__(self, wrapped):
-        self.wrapped = wrapped
-        self.offset = 0
-
-    def __next__(self):
-        if self.offset >= len(self.wrapped):
-            raise StopIteration
-        else:
-            item = self.wrapped[self.offset]
-            self.offset +=2
-            return item
+        offset = 0
+        while offset < len(self.wrapped):
+            item = self.wrapped[offset]
+            offset += 2
+            yield item
 
 
 class Squares:
@@ -35,6 +24,27 @@ class Squares:
             yield value ** 2
 
 
+class Iters:
+
+    def __init__(self, value):
+        self.data = value
+
+    def __getitem__(self, i):
+        print('get[%s]:' % i, end='')
+        return self.data[i]
+
+    def __iter__(self):
+        print('iter => next: ', end='')
+        for x in self.data:
+            yield x
+            print('next:', end='')
+
+
+    def __contains__(self, x):
+        print('contains: ', end='')
+        return x in self.data
+
+
 if __name__ == '__main__':
     alpha = 'abcde'
     skipper = SkipObject(alpha)
@@ -44,3 +54,19 @@ if __name__ == '__main__':
     for x in skipper:
         for y in skipper:
             print(x + y, end=' ')
+
+    X = Iters([1, 2, 3, 4, 5])
+    print(3 in X)
+    for i in X:
+        print(i, end=' | ')
+
+    print()
+    print([i ** 2 for i in X])
+    print(list(map(bin, X)))
+
+    I = iter(X)
+    while True:
+        try:
+            print(next(I), end=' @ ')
+        except StopIteration:
+            break
