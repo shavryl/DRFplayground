93667b696a561e52b49171ae50a5ca903da1e3dc
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2018-06-20 07:46:21 +0300

    iter and yield overloading

--- games/classmethods/class_overloading.py
+++ games/classmethods/class_overloading.py
@@ -1,6 +1,46 @@
 
 
-class StepperIndex:
+class SkipObject:
 
-    def __getitem__(self, item):
-        return self.data[item]
+    def __init__(self, wrapped):
+        self.wrapped = wrapped
+
+    def __iter__(self):
+        return SkipIterator(self.wrapped)
+
+
+class SkipIterator:
+
+    def __init__(self, wrapped):
+        self.wrapped = wrapped
+        self.offset = 0
+
+    def __next__(self):
+        if self.offset >= len(self.wrapped):
+            raise StopIteration
+        else:
+            item = self.wrapped[self.offset]
+            self.offset +=2
+            return item
+
+
+class Squares:
+
+    def __init__(self, start, stop):
+        self.start = start
+        self.stop = stop
+
+    def __iter__(self):
+        for value in range(self.start, self.stop + 1):
+            yield value ** 2
+
+
+if __name__ == '__main__':
+    alpha = 'abcde'
+    skipper = SkipObject(alpha)
+    I = iter(skipper)
+    print(next(I), next(I), next(I))
+
+    for x in skipper:
+        for y in skipper:
+            print(x + y, end=' ')
