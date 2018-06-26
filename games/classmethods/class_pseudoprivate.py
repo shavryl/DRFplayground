b70a4f36fb187015d03c51e09fb549a3f6254888
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2018-06-26 07:56:35 +0300

    pseudoprivate attributes

---
+++ games/classmethods/class_pseudoprivate.py
@@ -0,0 +1,18 @@
+
+
+class C1:
+
+    def meth1(self): self.__X = 88
+
+    def meth2(self): print(self.__X)
+
+
+class C2:
+
+    def metha(self): self.__X = 99
+
+    def methb(self): print(self.__X)
+
+
+class C3(C1, C2): pass
+
