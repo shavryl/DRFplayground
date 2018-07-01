144f6aa7af5e2f19be52e703929b1c28b3cdd358
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2018-07-01 13:43:42 +0300

    callables and unbound methods

--- games/classmethods/class_pseudoprivate.py
+++ games/classmethods/class_pseudoprivate.py
@@ -57,3 +57,75 @@ class Spam:
         print(message)
 
 
+class Number:
+
+    def __init__(self, base):
+        self.base = base
+
+    def double(self):
+        return self.base * 2
+
+    def tripple(self):
+        return self.base * 3
+
+
+def square(arg):
+    return arg ** 2
+
+
+class Sum:
+
+    def __init__(self, val):
+        self.val = val
+
+    def __call__(self, arg):
+        return self.val + arg
+
+
+class Product:
+
+    def __init__(self, val):
+        self.val = val
+
+    def method(self, arg):
+        return self.val * arg
+
+
+class Negate:
+
+    def __init__(self, val):
+        self.val = -val
+
+    def __repr__(self):
+        return str(self.val)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
