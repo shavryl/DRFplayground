34c1b6490310a5487fa831c014ea59454202895b
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2018-07-06 09:58:04 +0300

    decorators func and class

--- games/classmethods/classtools.py
+++ games/classmethods/classtools.py
@@ -127,3 +127,24 @@ class Set(list):
 
     def __repr__(self):
         return 'Set:' + list.__repr__(self)
+
+
+class traser:
+
+    def __init__(self, func):
+        self.calls = 0
+        self.func = func
+
+    def __call__(self, *args):
+        self.calls += 1
+        print('call %s to %s' % (self.calls, self.func.__name__))
+        return self.func(*args)
+
+
+def tracer(func):
+    def oncall(*args):
+        oncall.calls += 1
+        print('call %s to %s' % (oncall.calls, func.__name__))
+        return func(*args)
+    oncall.calls = 0
+    return oncall
