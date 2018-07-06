b3e3509f1dd55a849e5f0f6374c10c42d14a357d
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2018-07-06 08:52:06 +0300

    overriden set-list

--- games/classmethods/classtools.py
+++ games/classmethods/classtools.py
@@ -87,3 +87,43 @@ class ListTree:
 def factory(aClass, *args, **kwargs):
 
     return aClass(*args, **kwargs)
+
+
+class MyList(list):
+
+    def __getitem__(self, offset):
+        print('(indexing %s at %s)' % (self, offset))
+        return list.__getitem__(self, offset -1)
+
+
+class Set(list):
+
+    def __init__(self, value = []):
+        list.__init__([])
+        self.concat(value)
+
+    def intersect(self, other):
+        res = []
+        for x in self:
+            if x in other:
+                res.append(x)
+        return Set(res)
+
+    def union(self, other):
+        res = Set(self)
+        res.concat(other)
+        return res
+
+    def concat(self, value):
+        for x in value:
+            if not x in self:
+                self.append(x)
+
+    def __and__(self, other):
+        return self.intersect(other)
+
+    def __or__(self, other):
+        return self.union(other)
+
+    def __repr__(self):
+        return 'Set:' + list.__repr__(self)
