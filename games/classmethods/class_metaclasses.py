419a89b10b37ab17cef2c8e200f81dc01dec2ed8
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2018-08-07 08:16:01 +0300

    metaclass object wo type

--- games/classmethods/class_metaclasses.py
+++ games/classmethods/class_metaclasses.py
@@ -12,10 +12,38 @@ class Eggs:
     ...
 
 
+class MetaTwo(type):
+
+    def __new__(meta, classname, supers, classdict):
+        print('In MetaTwo.new:', meta, classname, supers, classdict, sep='\n...')
+        return type.__new__(meta, classname, supers, classdict)
+
+    def __init__(Class, classname, supers, classdict):
+        print('In MetaTwo.init:', classname, supers, classdict, sep='\n...')
+        print('...init class object:', list(Class.__dict__.keys()))
+
+
+class MetaObj:
+
+    def __call__(self, classname, supers, classdict):
+        print('In MetaObj.call:', classname, supers, classdict, sep='\n...')
+        Class = self.__New__(classname, supers, classdict)
+        self.__Init__(Class, classname, supers, classdict)
+        return Class
+
+    def __New__(self, classname, supers, classdict):
+        print('In MetaObj.new:', classname, supers, classdict, sep='\n...')
+        return type(classname, supers, classdict)
+
+    def __Init__(self, Class, classname, supers, classdict):
+        print('In MetaObj.init:', classname, supers, classdict, sep='\n...')
+        print('...init class object:', list(Class.__dict__.keys()))
+
+
 print('making class')
 
 
-class Spam(Eggs, metaclass=MetaOne):
+class Spam(Eggs, metaclass=MetaTwo):
     data = 1
 
     def meth(self, arg):
