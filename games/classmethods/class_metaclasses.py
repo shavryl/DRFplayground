91e8a60417c1509c84dcb1d5d831437c7ef8a433
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2018-08-08 07:55:08 +0300

    decorate all with arg

--- games/classmethods/class_metaclasses.py
+++ games/classmethods/class_metaclasses.py
@@ -142,7 +142,23 @@ class MetaTracer(type):
         return type.__new__(meta, classname, supers, classdict)
 
 
-class Person(metaclass=MetaTracer):
+# metaclass factory, apply any decorator to all methods of class
+def decorateAll(decorator):
+
+    class MetaDecorate(type):
+
+        def __new__(meta, classname, supers, classdict):
+
+            for attr, attrval in classdict.items():
+                if type(attrval) is FunctionType:
+                    classdict[attr] = decorator(attrval)
+
+            return type.__new__(meta, classname, supers, classdict)
+
+    return MetaDecorate
+
+
+class Person(metaclass=decorateAll(tracer)):
 
     def __init__(self, name, pay):
         self.name = name
