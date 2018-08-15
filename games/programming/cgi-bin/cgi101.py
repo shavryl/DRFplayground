eeae402f48e4f0c6a72eb203ce52f0dd8a37fef4
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2018-08-15 07:27:58 +0300

    doc string formatter more

--- games/programming/cgi-bin/cgi101.py
+++ games/programming/cgi-bin/cgi101.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
 import cgi
+import html
+
 form = cgi.FieldStorage()                 # parse form data
 print('Content-type: text/html\n')        # hdr plus blank line
 print('<title>Reply Page</title>')        # html reply page
 if not 'user' in form:
     print('<h1>Who are you?</h1>')
 else:
-    print('<h1>Hello <i>%s</i>!</h1>' % cgi.escape(form['user'].value))
+    print('<h1>Hello <i>%s</i>!</h1>' % html.escape(form['user'].value))
