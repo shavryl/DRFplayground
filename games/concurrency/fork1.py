aad0204843787e2dbf9510130edff5e542fc60f0
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2018-08-20 12:17:10 +0300

    concurrency fork1

---
+++ games/concurrency/fork1.py
@@ -0,0 +1,21 @@
+
+import os
+
+
+def child():
+    print('Hello from child', os.getpid())
+    os._exit(0)
+
+
+def parent():
+
+    while True:
+        newpid = os.fork()
+        if newpid == 0:
+            child()
+        else:
+            print('Hello from parent', os.getpid(), newpid)
+        if input() == 'q':
+            break
+
+parent()
