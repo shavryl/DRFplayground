56df5bc99451db5a88160cf7ea6debbd28b5508a
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2018-09-01 07:43:22 +0300

    daemon threads example

---
+++ games/concurrency/daemon_thread.py
@@ -0,0 +1,35 @@
+import threading, time
+
+
+def standart_thread():
+    print("Starting my thread")
+    time.sleep(20)
+    print("End standart thread")
+
+
+def daemon_thread():
+    while True:
+        print('Sended hartbeat from first daemon')
+        time.sleep(2)
+
+
+def daemon_thread2():
+    while True:
+        print('Sended hartbeat from second daemon')
+        time.sleep(1)
+
+
+def main():
+    standart = threading.Thread(target=standart_thread)
+    daemon = threading.Thread(target=daemon_thread)
+    daemon2 = threading.Thread(target=daemon_thread2)
+
+    daemon.setDaemon(True)
+    daemon2.setDaemon(True)
+
+    daemon.start()
+    daemon2.start()
+
+    standart.start()
+
+main()
