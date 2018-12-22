8a84c0bbc812165d894918a547b0dbe92d29334d
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2018-12-22 17:48:35 +0200

    ftp download remote

---
+++ games/networking/ftp/asciidl.py
@@ -0,0 +1,26 @@
+import os
+from ftplib import FTP
+
+
+# Downloads README from remote and writes it to disk.
+
+
+def main():
+    if os.path.exists('README'):
+        raise IOError('refusing to overwrite your README file')
+
+    ftp = FTP('ftp.kernel.org')
+    ftp.login()
+    ftp.cwd('/pub/linux/kernel')
+
+    with open('README', 'w') as f:
+        def writeline(data):
+            f.write(data)
+            f.write(os.linesep)
+
+        ftp.retrlines('RETR README', writeline)
+    ftp.quit()
+
+
+if __name__ == '__main__':
+    main()
